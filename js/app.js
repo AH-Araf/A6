c53a12a6d95a6a981229092fc3206879d49f24bf
Araf <arafathossain.baiust@gmail.com>
2022-09-20 01:56:09 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-/* fetching data from 1st api link to set navbar */
 const dataLoad = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
     displayData(data.data);
 }
-/* displaying dynamically navbar loading */
 const displayData = (Dataobject) => {
-    // console.log(objData.news_category);
     Dataobject.news_category.forEach(element => {
         const ulSel = document.getElementById('ul-li');
         const creLi = document.createElement('li');
